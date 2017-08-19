e8f47bd3d2294610390b8f6a8ab7ad9a6aa9ea00
jaredhuling <huling@wisc.edu>
2017-08-19 16:45:00 -0400

    default change

--- inst/reveal.js-3.3.0.1/js/reveal.js
+++ inst/reveal.js-3.3.0.1/js/reveal.js
@@ -39,8 +39,8 @@
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: 1371,
-			height: 1000,
+			width: 1714,
+			height: 1250,
 
 			// Factor of the display size that should remain empty around the content
 			margin: 0.1,
